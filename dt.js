6d57999dbf1e1114739d1a32ef1eb816ca69b0b0
Sounak <ssounak703@gmail.com>
2026-09-18 07:00:33 +0000

    files added

---
+++ dt.js
@@ -0,0 +1,16 @@
+//Reference(non-primitive data types)
+//arrays
+const heroes = ["spiderman","superman","ironman"]
+
+//objects
+let obj = {
+    name: 'sounak',
+    age:18,
+}
+console.log([obj.name,obj.age])
+
+//function
+const myFunction = function(){
+    console.log("hello world")
+}
+myFunction()
