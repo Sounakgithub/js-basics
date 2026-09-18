08222da335876023dc927634c97dc1ca0987aed6
Sounak <ssounak703@gmail.com>
2026-09-18 05:42:36 +0000

    commit the changes

---
+++ convops.js
@@ -0,0 +1,11 @@
+//let age=null
+//console.log(typeof age)
+//let num = Number(age)
+//console.log(typeof(num))
+//console.log(num)
+//const loggedIn = 1
+//const isLoggedIn = Boolean(loggedIn)
+//console.log(isLoggedIn)
+
+console.log(1+2+"2")
+console.log("2"+1+2)
