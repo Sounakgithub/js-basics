7e95c634360745091cfc79c3ba204c183b55116a
Sounak <ssounak703@gmail.com>
2026-09-19 13:41:52 +0000

    more objects

---
+++ obj1.js
@@ -0,0 +1,19 @@
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+//combine two objects using Object.assign()
+const obj3 = Object.assign({},obj1,obj2)
+console.log(obj3)
+
+//when array of objects come from a database
+const users = [
+    {
+        id:1,
+        email:"s@123"
+    },
+    {
+        id:2,
+        email:"a@123"
+    }
+]
+console.log(users[0].id)
