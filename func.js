26080493c8d27615f0fe368dfca433f007000cde
Sounak <ssounak703@gmail.com>
2026-09-19 15:12:56 +0000

    functions added

---
+++ func.js
@@ -0,0 +1,9 @@
+function loggedIn(username="sam"){ //giving a defaut value
+    /*if(!username){
+        console.log("please enter an username")
+        return
+    }*/
+    return `${username} just logged in!`
+}
+console.log(loggedIn("sounak"))
+console.log(loggedIn())
