9692d040e3919b416deb60db82f58f509a171583
Sounak <ssounak703@gmail.com>
2026-09-19 14:15:34 +0000

    more added

--- obj2.js
+++ obj2.js
@@ -9,3 +9,8 @@ const course = {
 //just another syntax...
 const{instructor,price} = course
 console.log(instructor,price)
+//use of the above in React(destructuring)
+/*const navbar = ({company,date})=>{
+
+}
+navbar(company = "sounak",date = "20-11-2026")*/
