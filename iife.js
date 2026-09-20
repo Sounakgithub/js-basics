e296ce82345ba023882bcbd6e3205d2860de5734
Sounak <ssounak703@gmail.com>
2026-09-20 07:43:40 +0000

    iife added

--- iife.js
+++ iife.js
@@ -1,1 +1,8 @@
 //IMMEDIATELY INVOKED FUNCTION EXPRESSIONS: To prevent the global scope from polluting other functions
+(function chai(){
+    //named iife
+    console.log("DB CONNECTED!")
+})();  /*USE SEMI COLON OTHERWISE ERROR WILL OCCUR */
+((name)=>{
+    console.log(`DB CONNECTED ${name} `)
+})("sounak");
